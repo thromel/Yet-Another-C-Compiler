34964f4804400e984217fe08e64811e3b0d3537d
thr0m3l <1705069@ugrad.cse.buet.ac.bd>
2020-03-12 05:08:31 +0600

    Added functioning main.cpp

--- Offline 1 - Symbol Table/SymbolTable/main.cpp
+++ Offline 1 - Symbol Table/SymbolTable/main.cpp
@@ -5,4 +5,79 @@ using namespace std;
 
 int main()
 {
+    SymbolTable st;
+
+    int length, number, current;
+    current = number = 0;
+
+    cin >> length;
+
+    st.enterScope(++number, length);
+    current++;
+
+    string choice, name, type;
+
+    while (true)
+    {
+        cin >> choice;
+
+        if (choice == "I")
+        {
+
+            cin >> name >> type;
+
+            cout << choice << " " << name << " " << type << "\n\n";
+
+            st.insertSymbol(name, type);
+        }
+        else if (choice == "L")
+        {
+            cin >> name;
+            cout << choice << " " << name << "\n\n";
+            st.lookup(name);
+        }
+        else if (choice == "D")
+        {
+            cin >> name;
+            cout << choice << " " << name << "\n\n";
+            st.deleteSymbol(name);
+        }
+        else if (choice == "P")
+        {
+            cin >> type;
+            if (type == "A" || type == "C")
+            {
+                cout << choice << " " << type << "\n\n";
+                if (type == "A")
+                    st.printAll();
+                else
+                    st.printCurrent();
+            }
+
+            else
+                cout << "Invalid operation\n\n";
+        }
+        else if (choice == "S")
+        {
+            cout << choice << "\n";
+            st.enterScope(++number, length);
+            current++;
+        }
+        else if (choice == "E")
+        {
+            cout << choice << "\n";
+
+            st.exitScope();
+            current--;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    for (int i = 0; i < current; ++i)
+    {
+        st.exitScope();
+    }
 }
