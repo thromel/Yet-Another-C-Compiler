23c729f010e111240b66094a2a05ddd34fa6b345
thr0m3l <1705069@ugrad.cse.buet.ac.bd>
2021-07-06 00:16:26 +0600

    Structured the folders

--- Offline 1 - Symbol Table/SymbolTable/main.cpp
+++ Offline 1 - Symbol Table/SymbolTable/main.cpp
@@ -1,92 +1,73 @@
-#include <iostream>
 #include "SymbolTable.h"
+#include <iostream>
 
 using namespace std;
 
-int main()
-{
-    freopen("output.txt", "w", stdout);
-    freopen("input5.txt", "r", stdin);
+int main(int argc, char **argv) {
+  freopen("output.txt", "w", stdout);
+  freopen(argv[1], "r", stdin);
 
-    SymbolTable st;
+  SymbolTable st;
 
-    int length, number, current;
-    current = 1;
-    number = 1;
+  int length, number, current;
+  current = 1;
+  number = 1;
 
-    cin >> length;
+  cin >> length;
 
-    st.enterScope(length);
-    current++;
+  st.enterScope(length);
+  current++;
 
-    string choice, name, type;
+  string choice, name, type;
 
-    while (true)
-    {
-        cout << ">> ";
-        cin >> choice;
+  while (true) {
+    cout << ">> ";
+    cin >> choice;
 
-        if (choice == "I")
-        {
+    if (choice == "I") {
 
-            cin >> name >> type;
+      cin >> name >> type;
 
-            cout << choice << " " << name << " " << type << "\n\n";
+      cout << choice << " " << name << " " << type << "\n\n";
 
-            st.insertSymbol(name, type);
-        }
-        else if (choice == "L")
-        {
-            cin >> name;
-            cout << choice << " " << name << "\n\n";
-            st.lookup(name);
-        }
-        else if (choice == "D")
-        {
-            cin >> name;
-            cout << choice << " " << name << "\n\n";
-            st.deleteSymbol(name);
-        }
-        else if (choice == "P")
-        {
-            cin >> type;
-            if (type == "A" || type == "C")
-            {
-                cout << choice << " " << type << "\n\n";
-                if (type == "A")
-                    st.printAll();
-                else
-                    st.printCurrent();
-            }
-
-            else
-                cout << "Invalid operation\n\n";
-        }
-        else if (choice == "S")
-        {
-            cout << choice << "\n";
-            st.enterScope(length);
-            current++;
-        }
-        else if (choice == "E")
-        {
-            cout << choice << "\n";
-
-            st.exitScope();
-            current--;
-        }
-        else if (choice == "T")
-        {
-            break;
-        }
+      st.insertSymbol(name, type);
+    } else if (choice == "L") {
+      cin >> name;
+      cout << choice << " " << name << "\n\n";
+      st.lookup(name);
+    } else if (choice == "D") {
+      cin >> name;
+      cout << choice << " " << name << "\n\n";
+      st.deleteSymbol(name);
+    } else if (choice == "P") {
+      cin >> type;
+      if (type == "A" || type == "C") {
+        cout << choice << " " << type << "\n\n";
+        if (type == "A")
+          st.printAll();
         else
-        {
-            cout << "Invalid operation";
-        }
-    }
+          st.printCurrent();
+      }
 
-    for (int i = 0; i < current; ++i)
-    {
-        st.exitScope();
+      else
+        cout << "Invalid operation\n\n";
+    } else if (choice == "S") {
+      cout << choice << "\n";
+      st.enterScope(length);
+      current++;
+    } else if (choice == "E") {
+      cout << choice << "\n";
+
+      st.exitScope();
+      current--;
+    } else if (choice == "T") {
+      break;
+    } else {
+      cout << "Invalid operation";
     }
+  }
+
+  for (int i = 0; i < current; ++i) {
+    st.exitScope();
+  }
 }
