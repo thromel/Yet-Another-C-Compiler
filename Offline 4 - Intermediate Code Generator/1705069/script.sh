74dd63b49d89924a57406494e30db1deab92e0c9
Tanzim Hossain Romel <romel.rcs@gmail.com>
2025-05-20 17:39:20 +0600

    Add initial parser files and Docker setup for Intermediate Code Generator

--- Offline 4 - Intermediate Code Generator/1705069/script.sh
+++ Offline 4 - Intermediate Code Generator/1705069/script.sh
@@ -4,26 +4,49 @@
 
 helpFunction()
 {
-   echo "Usage: ./script.sh filename.c"
-   exit 1 # Exit script after printing help
+    echo "Usage: ./script.sh filename.c"
+    exit 1 # Exit script after printing help
 }
 
 if [ -z "$1" ]
 then
-   echo "File name is not given";
-   helpFunction
+    echo "File name is not given";
+    helpFunction
 fi
 
-bison -d -y -v 1705069.y
+bison -d -y -v ./17./sc05069.y
+if [ $? -ne 0 ]; then
+    echo 'Error: Bison parser generation failed'
+    exit 1
+fi
 echo 'Generated the parser C file as well the header file'
+
 g++ -w -c -o y.o y.tab.c
+if [ $? -ne 0 ]; then
+    echo 'Error: Parser compilation failed'
+    exit 1
+fi
 echo 'Generated the parser object file'
+
 flex 1705069.l
+if [ $? -ne 0 ]; then
+    echo 'Error: Flex scanner generation failed'
+    exit 1
+fi
 echo 'Generated the scanner C file'
+
 g++ -w -c -o l.o lex.yy.c
-# if the above command doesn't work try g++ -fpermissive -w -c -o l.o lex.yy.c
+if [ $? -ne 0 ]; then
+    echo 'Error: Scanner compilation failed'
+    exit 1
+fi
 echo 'Generated the scanner object file'
+
 g++ y.o l.o -lfl
+if [ $? -ne 0 ]; then
+    echo 'Error: Final linking failed'
+    exit 1
+fi
 echo 'All ready, running'
 ./a.out $1
 
